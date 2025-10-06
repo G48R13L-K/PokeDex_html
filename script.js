31c3563d60a4f6870fa00f6403ab1b6c9f3f3893
GABRIELAUGUSTOKAPPENBERG <kappenberg4@gmail.com>
2025-10-06 19:45:40 -0300

    correção

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ console.log("rodou função")
         li.addEventListener("click",()=>{
            //JSON.
             localStorage.setItem("pokemon",JSON.stringify(infos))
-            location.href = "./pokemon"
+            location.href = "pokemon.html"
         })
 
         const img = document.createElement("img")
